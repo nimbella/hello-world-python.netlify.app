c0314751b49d557ce562bc5dd5013f7324f06e06
rodric rabbah <rodric@gmail.com>
2020-10-04 14:19:30 -0400

    Update index.html/default name.

--- packages/default/greet.py
+++ packages/default/greet.py
@@ -1,5 +1,7 @@
 def main(args):
-    message = "Hello, " + args.get("name", "World") + "!";
+    name = args.get('name', "World")
+    name = name if name else "World"
+    message = "Hello, " + name + "!";
     print(message)
     return {
         "body": {"message": message}
